8570c76a646749d336a05526a2a610d86d13d257
Kevin Kirsche <kevin.kirsche@one.verizon.com>
2023-09-08 16:56:28 +0000

    feat: Add support for ApiRootModel

--- src/apispec_pydantic_plugin/models.py
+++ src/apispec_pydantic_plugin/models.py
@@ -1,11 +1,17 @@
 from typing import TypeAlias
 
-from pydantic import BaseModel
+from pydantic import BaseModel, RootModel
 
 from apispec_pydantic_plugin.registry import Registry
 
 
 class ApiBaseModel(BaseModel):
+    def __init_subclass__(cls) -> None:
+        Registry.register(cls)
+        return super().__init_subclass__()
+
+
+class ApiRootModel(RootModel):
     def __init_subclass__(cls) -> None:
         Registry.register(cls)
         return super().__init_subclass__()
