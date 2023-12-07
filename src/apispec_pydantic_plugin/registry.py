417891fe72f8a27a6830ca3401888191507bac5a
Kevin Kirsche <kevin.kirsche@one.verizon.com>
2023-12-07 09:20:22 -0500

    use future annotations

--- src/apispec_pydantic_plugin/registry.py
+++ src/apispec_pydantic_plugin/registry.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from typing import TYPE_CHECKING, ClassVar
 
 from apispec_pydantic_plugin.errors import ModelNotFoundError
@@ -9,17 +11,17 @@ if TYPE_CHECKING:
 class Registry:
     # we're using BaseModel instead of ApiBaseModel so that users may manually
     # register classes with the registry, if they choose.
-    registered: ClassVar[dict[str, type["BaseModelAlias"]]] = {}
+    registered: ClassVar[dict[str, type[BaseModelAlias]]] = {}
 
     @classmethod
-    def register(cls, model: type["BaseModelAlias"]) -> None:
+    def register(cls, model: type[BaseModelAlias]) -> None:
         name = model.__name__
         if name in cls.registered:
             raise ValueError(f"Duplicate schema received by registry: {name}")
         cls.registered[name] = model
 
     @classmethod
-    def get(cls, name: str) -> type["BaseModelAlias"]:
+    def get(cls, name: str) -> type[BaseModelAlias]:
         try:
             return cls.registered[name]
         except KeyError as e:
